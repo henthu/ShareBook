e5f2adab48d088d827e8c5d182273eb9a9ff83b8
Mohamed Amine HAMDI <mohamedamine.hamdi@gmail.com>
2016-03-04 15:24:30 +0100

    routing + home controller in use

---
+++ Sharebook/Models/Book.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Sharebook.Models
+{
+    class Book
+    {
+        public string Name { get; set; }
+        public string Author { get; set; }
+        public int ID {get;set;}
+        
+    }
+    
+}
