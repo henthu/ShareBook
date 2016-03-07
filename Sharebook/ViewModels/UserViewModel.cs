26e8b578d92eb92291720d4ccb392dfab5dff962
Amine HAMDI <mohamedamine.hamdi@gmail.com>
2016-03-07 12:56:54 +0100

    files added

---
+++ Sharebook/ViewModels/UserViewModel.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sharebook.ViewModels
+{
+   public class UserViewModel
+    {
+        [Required]
+        [StringLength(50,MinimumLength =5)]
+        public string Login { get; set; }
+        [Required]
+        [StringLength(50,MinimumLength =3)]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(250,MinimumLength =8)]
+        [RegularExpression()]
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+    }
+   
+}
